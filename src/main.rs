d2a0bb0a578872c86478759b8869d8a99e201006
YINCHU XIA <larry.fantasy@gmail.com>
2019-09-26 22:17:23 -0700

    fix ip

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() -> std::io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     let port = env::var("PORT").unwrap_or("8080".to_string());
-    let addr = format!("127.0.0.1:{}", port);
+    let addr = format!("0.0.0.0:{}", port);
 
     HttpServer::new(|| {
         App::new()
