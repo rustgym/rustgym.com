ea2cadc7bf55c485c18f5d30dd0a21d30a43b10b
YINCHU XIA <larry.fantasy@gmail.com>
2019-09-30 11:17:50 -0700

    test db

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
+    title: 'RustGym',
+    description: 'The best gym for coding',
     siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
   },
   mapping: {
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,42 @@
+#[macro_use]
+extern crate diesel;
 use actix_web::{middleware, web, App, HttpRequest, HttpServer};
 use actix_files as fs;
 use std::env;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use postgres::{Connection, TlsMode};
+
+// pub fn establish_connection() -> PgConnection {
+//     let database_url = env::var("DATABASE_URL").unwrap();
+//     PgConnection::establish(&database_url)
+//         .expect(&format!("Error connecting to {}", database_url))
+// }
+
 
 fn main() -> std::io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     let port = env::var("PORT").unwrap_or("8080".to_string());
     let addr = format!("0.0.0.0:{}", port);
-
     HttpServer::new(|| {
         App::new()
             // enable logger
             .wrap(middleware::Logger::default())
+            .service(web::resource("/db").to(|| {
+                let database_url = env::var("DATABASE_URL").unwrap();
+                let conn = Connection::connect(database_url, TlsMode::None).unwrap();
+                let mut s = "".to_string();
+                for row in &conn.query("SELECT * FROM guestbook", &[]).unwrap() {
+                    let a: String = row.get(0);
+                    let b: String = row.get(1);
+                    s += &a;
+                    s += "===";
+                    s += &b;
+                    s += "+++";
+                }
+                s
+            }))
             .service(fs::Files::new("/", "static").index_file("index.html"))
     })
     .bind(addr)?
